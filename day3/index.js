faa85c6a9dc9cc12b93df3ef8443ed39e193c747
Tiffany Hoang <tiffany.hoang@myob.com>
2021-12-06 19:31:19 +1100

    refactor day3-part1

--- day3/index.js
+++ day3/index.js
@@ -1,47 +1,27 @@
-// 00100
-// 11110
-// 10110
-// 10111
-// 10101
-// 01111
-// 00111
-// 11100
-// 10000
-// 11001
-// 00010
-// 01010
-
-// gammaRate = 10110 = 22 (the most common bits)
-// epsilonRate = 01001 = 9 (the least common bits)
-// power consumption = gamma rate x the epsilon rate
-// 011111101011: there are five 0 bits and seven 1 bits.
-
 import fs from "fs";
 const data = fs.readFileSync("./inputs.txt");
 const inputs = data.toString("utf-8").split("\n");
 
 const calculatePowerConsumption = (inputs) => {
   const numberOfBits = inputs[0].length;
-  let count0BitsArray = [];
-  let count1BitsArray = [];
-  let gammaRate = [];
-  let epsilonRate = [];
-  for (let i = 0; i < numberOfBits; i++) {
-    count0BitsArray[i] = 0;
-    count1BitsArray[i] = 0;
-    gammaRate[i] = 0;
-    epsilonRate[i] = 0;
-  }
+  const count0BitsArray = Array(numberOfBits).fill(0);
+  const count1BitsArray = Array(numberOfBits).fill(0);
 
   inputs.forEach((input) => {
     const bitsArray = input.split("").map((bit) => parseInt(bit));
     for (let i = 0; i < numberOfBits; i++) {
       count0BitsArray[i] += bitsArray[i] === 0 ? 1 : 0;
       count1BitsArray[i] += bitsArray[i] === 0 ? 0 : 1;
-
-      gammaRate[i] = count0BitsArray[i] > count1BitsArray[i] ? 0 : 1;
-      epsilonRate[i] = count0BitsArray[i] > count1BitsArray[i] ? 1 : 0;
     }
+  });
+
+  const gammaRate = [];
+  const epsilonRate = [];
+
+  count0BitsArray.forEach((bit, index) => {
+    gammaRate[index] = count0BitsArray[index] > count1BitsArray[index] ? 0 : 1;
+    epsilonRate[index] =
+      count0BitsArray[index] > count1BitsArray[index] ? 1 : 0;
   });
 
   const gammaRateInDecimal = parseInt(gammaRate.join(""), 2);
